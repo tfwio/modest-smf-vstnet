db8bc73cf3900edeb3a205c2b55b2342108192db
tfwio <tfwroble@gmail.com>
2017-06-12 00:37:29 -0500

    remove unusable erratum

--- Source/gen.snd.vstsmfui/Source/Common.Extensions/MidiTree.cs
+++ Source/gen.snd.vstsmfui/Source/Common.Extensions/MidiTree.cs
@@ -1,21 +1,13 @@
 ﻿/*
- * Created by SharpDevelop.
  * User: tfooo
  * Date: 11/12/2005
  * Time: 4:19 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 using System.Windows.Forms;
 using gen.snd;
-using gen.snd.Vst;
 using gen.snd.Vst.Module;
-using Jacobi.Vst.Core;
-using NAudio.Wave;
 
-// Gram Hancock.com: legend of spinx , arc of the covenent, quest for civilization, underworld?
-// lost kingdoms of the ice age
 namespace modest100.Forms
 {
 	static class MidiTree
